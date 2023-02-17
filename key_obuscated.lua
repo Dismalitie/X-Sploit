905f05f481d1a1d9e891c9635aaddae65a5d4617
Dismalitie <118924562+Dismalitie@users.noreply.github.com>
2023-02-17 11:58:20 +0000

    Update key_obuscated.lua
    added scrypt

--- key_obuscated.lua
+++ key_obuscated.lua
@@ -1,5 +1,12 @@
 local a = "LpDmXEmIN4sNsyltLoja"
-local key = a
-local a = key
-return key
---[[scrypt: key = pull:from%string(local{in.project} math.random(specify?integer)) 
+return a
+--[[scrypt: key = pull:from%string(local{in.project} math.random(specify?integer))
+      if workspace:a?input == "LpDmXEmIN4sNsyltLoja" do
+        print("u read the code. but you still got it wrong lol")
+      end
+      lol = find:{in.project} lolvariable
+      if {in.current} not find:({scrypt.tag}) do
+        print("u thought removing scrypt would work? try harder")
+      end
+return scrypt
+--]]
